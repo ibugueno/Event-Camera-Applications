679c58cdbcc6fd267e1d67bc938da531a4915393
Ignacio Bugueño <ignacio@MacBook-Pro-de-Ignacio.local>
2020-12-11 12:19:15 -0300

    full erl

--- event_representation_learning/run_docker.sh
+++ event_representation_learning/run_docker.sh
@@ -2,6 +2,6 @@
 
 docker rm -fv ignacio_erl
 
-nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=0  --name ignacio_erl -v /home/ignacio.bugueno/cachefs/erl/input:/app/input -v /home/ignacio.bugueno/cachefs/erl/output:/app/output ignacio_erl
+nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=0  --name ignacio_erl -v /home/ignacio.bugueno/cachefs/erl/input:/app/input -v /home/ignacio.bugueno/cachefs/erl/output:/app/output -v /home/ignacio.bugueno/cachefs/erl/log:/app/log ignacio_erl
 
 sleep 10; docker logs ignacio_erl
