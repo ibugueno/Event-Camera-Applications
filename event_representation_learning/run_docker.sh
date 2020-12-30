f03bf9001a917a736467335839de5efb5556ce9b
Ignacio Bugueno <ignacio.bugueno@live.com>
2020-12-29 23:45:36 -0300

    GPU

--- event_representation_learning/run_docker.sh
+++ event_representation_learning/run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker rm -fv ignacio_erl
+#docker rm -fv ignacio_erl
 
-nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=0  --name ignacio_erl -v /home/ignacio.bugueno/cachefs/erl/input:/app/input -v /home/ignacio.bugueno/cachefs/erl/output:/app/output -v /home/ignacio.bugueno/cachefs/erl/log:/app/log ignacio_erl
+nvidia-docker run -it -e NVIDIA_VISIBLE_DEVICES=2  --name ignacio_erl -v /home/ignacio.bugueno/cachefs/erl/input:/app/input -v /home/ignacio.bugueno/cachefs/erl/output:/app/output -v /home/ignacio.bugueno/cachefs/erl/log:/app/log ignacio_erl
 
-sleep 10; docker logs ignacio_erl
+#sleep 10; docker logs ignacio_erl
